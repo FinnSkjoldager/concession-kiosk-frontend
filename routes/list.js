5a97d302badf6504fbd51b3fa2878185cebf81f2
Finn Skjoldager <finn.skjold@gmail.com>
2021-08-02 16:32:11 +0200

    Update list.js

--- routes/list.js
+++ routes/list.js
@@ -3,13 +3,13 @@ const router = express.Router();
 var request = require('request');
 
 /* GET request for thank you page */
+
   request.get('/allorders', function( err, response, body) {
           if(err){
             console.log("FRONTEND ERROR GET /ALLORDERS");
           } else {
             let orders = JSON.parse(body);
             res.render('list', {orders});
-            
           };  
   });
 module.exports = router;
