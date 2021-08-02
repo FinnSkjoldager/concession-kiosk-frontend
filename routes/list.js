1dc553a3c7c492234cc3d8e9bd81df477b41c814
Finn Skjoldager <finn.skjold@gmail.com>
2021-08-02 15:44:50 +0200

    Update list.js

--- routes/list.js
+++ routes/list.js
@@ -2,18 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 /* GET request for thank you page */
-router.get('/list', function(req, res, next) {
-  console.log('req: ' + req);
-    request.get('/allorders', function( err, response, body) {
-      if(err){
-        console.log("FRONTEND ERROR GET /ALLORDERS");
-      } else {
-        let orders = JSON.parse(body);
-        res.render('list', {orders});
-        
-      }  
-  }
-  res.render('list', { ticketNumber: 200 });
+  request.get('/allorders', function( err, response, body) {
+          if(err){
+            console.log("FRONTEND ERROR GET /ALLORDERS");
+          } else {
+            let orders = JSON.parse(body);
+            res.render('list', {orders});
+            
+          };  
+  };
 });
 module.exports = router;
 
