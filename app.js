8b0f581680d55c6ea65c73669d7ddb1d77ec81bb
Finn Skjoldager <finn.skjold@gmail.com>
2021-08-02 16:50:18 +0200

    ok
    ok

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/a', indexRouter);
 app.use('/thankyou', thankyouRouter);
-app.use('/list', listRouter);
+app.use('/', listRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
