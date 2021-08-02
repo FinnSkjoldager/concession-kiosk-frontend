e2753957e582c1bbddd15a01ad7f248f6156f14b
Finn Skjoldager <finn.skjold@gmail.com>
2021-08-02 16:18:14 +0200

    Update list.js

--- routes/list.js
+++ routes/list.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+var request = require('request');
 
 /* GET request for thank you page */
   request.get('/allorders', function( err, response, body) {
