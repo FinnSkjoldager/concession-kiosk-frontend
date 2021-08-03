faa6065029d96ee2ccbd0598684f681defdce3f4
Finn Skjoldager <finn.skjold@gmail.com>
2021-08-03 18:07:14 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var request = require('request');
 var host = process.env.COMPONENT_BACKEND_HOST || '0.0.0.0';
 var port = process.env.COMPONENT_BACKEND_PORT || 8080;
+host = '172.30.237.8';
 console.log("PORT "+port);
 /* GET main page */
 router.get('/', function(req, res, next) {
